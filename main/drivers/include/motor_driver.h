6e7c6d9629e887dfa772a4aa08f884a7d4bb231b
Ronny Majani <ronnymajani@gmail.com>
2017-09-16 19:43:17 +0300

    Added Basic Low Level Motor Driver functions (UNTESTED)

--- main/drivers/include/motor_driver.h
+++ main/drivers/include/motor_driver.h
@@ -8,24 +8,57 @@
 #ifndef MAIN_DRIVERS_INCLUDE_MOTOR_DRIVER_H_
 #define MAIN_DRIVERS_INCLUDE_MOTOR_DRIVER_H_
 
-// GPIO Pin Assignments
-#define MOTOR_DRIVER1_GPIO_IN1_IN3
-#define MOTOR_DRIVER1_GPIO_IN2_IN4
-#define MOTOR_DRIVER1_GPIO_PWM
+#include "driver/ledc.h"
 
-#define MOTOR_DRIVER2_GPIO_IN1_IN3
-#define MOTOR_DRIVER2_GPIO_IN2_IN4
-#define MOTOR_DRIVER2_GPIO_PWM
+// Low Level Pin Assignments
+#define MOTOR_DRIVER_GPIO_IN1		16
+#define MOTOR_DRIVER_GPIO_IN2		17
+#define MOTOR_DRIVER_GPIO_PWM1		04
+#define MOTOR_DRIVER_PWM1_CHANNEL	LEDC_CHANNEL_0
+
+#define MOTOR_DRIVER_GPIO_IN3		18
+#define MOTOR_DRIVER_GPIO_IN4		19
+#define MOTOR_DRIVER_GPIO_PWM2		05
+#define MOTOR_DRIVER_PWM2_CHANNEL	LEDC_CHANNEL_1
+
+#define MOTOR_DRIVER_PWM_MODE	LEDC_LOW_SPEED_MODE
+#define MOTOR_DRIVER_PWM_TIMER	LEDC_TIMER_0
+#define MOTOR_DRIVER_PWM_FREQ	5000
+#define MOTOR_DRIVER_PWM_BITNUM	LEDC_TIMER_10_BIT
+#define MOTOR_DRIVER_MAX_PWM	1023	// (duty range is 0 ~ ((2**bit_num)-1)
 
 
-// Motor Pair Specific Pin Assignments
-#define MOTOR_RIGHT_CW	MOTOR_DRIVER1_GPIO_IN1_IN3
-#define MOTOR_RIGHT_CCW	MOTOR_DRIVER1_GPIO_IN2_IN4
-#define MOTOR_RIGHT_SPD	MOTOR_DRIVER1_GPIO_PWM
+// Motor Abstracted Pin Assignments
+#define MOTOR_RIGHT_F_GPIO		MOTOR_DRIVER_GPIO_IN3	// Forward
+#define MOTOR_RIGHT_R_GPIO		MOTOR_DRIVER_GPIO_IN4	// Reverse
+#define MOTOR_RIGHT_PWM_GPIO	MOTOR_DRIVER_GPIO_PWM2
+#define MOTOR_RIGHT_PWM_CHANNEL	MOTOR_DRIVER_PWM2_CHANNEL
+#define MOTOR_RIGHT_GPIO_PIN_SELECT	((1<<MOTOR_RIGHT_F_GPIO) | (1<<MOTOR_RIGHT_R_GPIO))
 
-#define MOTOR_LEFT_CW	MOTOR_DRIVER2_GPIO_IN1_IN3
-#define MOTOR_LEFT_CCW	MOTOR_DRIVER2_GPIO_IN2_IN4
-#define MOTOR_LEFT_SPD	MOTOR_DRIVER2_GPIO_PWM
+#define MOTOR_LEFT_F_GPIO		MOTOR_DRIVER_GPIO_IN1	// Forward
+#define MOTOR_LEFT_R_GPIO		MOTOR_DRIVER_GPIO_IN2	// Reverse
+#define MOTOR_LEFT_PWM_GPIO		MOTOR_DRIVER_GPIO_PWM1
+#define MOTOR_LEFT_PWM_CHANNEL	MOTOR_DRIVER_PWM1_CHANNEL
+#define MOTOR_LEFT_GPIO_PIN_SELECT	((1<<MOTOR_LEFT_F_GPIO) | (1<<MOTOR_LEFT_R_GPIO))
+
+
+
+/* Exported Functions */
+void set_motor_right_direction_forward();
+void set_motor_right_direction_reverse();
+int set_motor_right_pwm(int pwm);
+void disable_motor_right();
+
+void set_motor_left_direction_forward();
+void set_motor_left_direction_reverse();
+int set_motor_left_pwm(int pwm);
+void disable_motor_left();
+
+void set_motors_direction_forward();
+void set_motors_direction_reverse();
+int set_motors_pwm(int pwm);
+void disable_motors();
+
 
 
 #endif /* MAIN_DRIVERS_INCLUDE_MOTOR_DRIVER_H_ */
--- main/drivers/motor_driver.c
+++ main/drivers/motor_driver.c
@@ -6,19 +6,132 @@
  */
 
 /**
- * Right Front Motor
- * Right Back Motor
- * Left Front Motor
- * Left Rear Motor
- *
- * Right Side (RF + RB)
- * Left Side (LF + LB)
+ * Right Side
+ * Left Side
  *
  * Forward(orientation)
  * Reverse(orientation)
  * Rotate in place
  * Break/Stop
  */
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#include "motor_driver.h"
+
+
+/**
+ * Initializes the pins needed for the Motor Driver and performs the necessary configuration
+ */
+void init_motor_driver() {
+	/* Initialize GPIO Pins */
+	gpio_config_t motor_pin_config;
+	motor_pin_config.intr_type = GPIO_PIN_INTR_DISABLE;
+	motor_pin_config.mode = GPIO_MODE_OUTPUT;
+	motor_pin_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
+	motor_pin_config.pull_up_en = GPIO_PULLUP_DISABLE;
+	motor_pin_config.pin_bit_mask = (MOTOR_RIGHT_GPIO_PIN_SELECT | MOTOR_LEFT_GPIO_PIN_SELECT);
+	gpio_config(&motor_pin_config);
+
+	/* Initialize PWM (LEDC module) */
+	// PWM Timer config
+	ledc_timer_config_t pwm_timer;
+	pwm_timer.bit_num = MOTOR_DRIVER_PWM_BITNUM;
+	pwm_timer.freq_hz = MOTOR_DRIVER_PWM_FREQ;
+	pwm_timer.speed_mode = MOTOR_DRIVER_PWM_MODE;
+	pwm_timer.timer_num = MOTOR_DRIVER_PWM_TIMER;
+	ledc_timer_config(&pwm_timer);
+
+	// PWM Channels config
+	ledc_channel_config_t pwm_channel;
+	pwm_channel.channel = MOTOR_RIGHT_PWM_CHANNEL;
+	pwm_channel.duty = 0;
+	pwm_channel.gpio_num = MOTOR_RIGHT_PWM_GPIO;
+	pwm_channel.intr_type = LEDC_INTR_DISABLE;
+	pwm_channel.timer_sel = MOTOR_DRIVER_PWM_TIMER;
+	pwm_channel.speed_mode = MOTOR_DRIVER_PWM_MODE;
+	ledc_channel_config(&pwm_channel);
+}
 
 
 
+/**
+ * Sets/Unsets the GPIO levels of the two given pins respectively
+ * @param motor_pin_X	the number of the pin to set to HIGH
+ * @param motor_pin_Y	the number of the pin to set to LOW
+ */
+static void set_pin_x_clear_pin_y(int motor_pin_X, int motor_pin_Y) {
+	gpio_set_level(motor_pin_X, 1);
+	gpio_set_level(motor_pin_Y, 0);
+}
+
+
+/**
+ * Sets the PWM Duty Cycle for the given Channel if the given value is within the legal range.
+ * Returns 1 if the PWM was changed, 0 if the given value is invalid
+ */
+static int set_pwm(ledc_channel_t channel, int pwm) {
+	if(pwm >= 0 && pwm <= MOTOR_DRIVER_MAX_PWM) {
+		ledc_set_duty(MOTOR_DRIVER_PWM_MODE, channel, pwm);
+		ledc_update_duty(MOTOR_DRIVER_PWM_MODE, channel);
+		return 1;
+	}
+	return 0;
+}
+
+
+/* Right Motors */
+void set_motor_right_direction_forward() {
+	set_pin_x_clear_pin_y(MOTOR_RIGHT_F_GPIO, MOTOR_RIGHT_R_GPIO);
+}
+
+void set_motor_right_direction_reverse() {
+	set_pin_x_clear_pin_y(MOTOR_RIGHT_R_GPIO, MOTOR_RIGHT_F_GPIO);
+}
+
+int set_motor_right_pwm(int pwm) {
+	return set_pwm(MOTOR_RIGHT_PWM_CHANNEL, pwm);
+}
+
+void disable_motor_right() {
+	gpio_set_level(MOTOR_RIGHT_F_GPIO, 0);
+	gpio_set_level(MOTOR_RIGHT_R_GPIO, 0);
+}
+
+/* Left Motors */
+void set_motor_left_direction_forward() {
+	set_pin_x_clear_pin_y(MOTOR_LEFT_F_GPIO, MOTOR_LEFT_R_GPIO);
+}
+
+void set_motor_left_direction_reverse() {
+	set_pin_x_clear_pin_y(MOTOR_LEFT_R_GPIO, MOTOR_LEFT_F_GPIO);
+}
+
+int set_motor_left_pwm(int pwm) {
+	return set_pwm(MOTOR_LEFT_PWM_CHANNEL, pwm);
+}
+
+void disable_motor_left() {
+	gpio_set_level(MOTOR_LEFT_F_GPIO, 0);
+	gpio_set_level(MOTOR_LEFT_R_GPIO, 0);
+}
+
+/* All Motors */
+void set_motors_direction_forward() {
+	set_motor_right_direction_forward();
+	set_motor_left_direction_forward();
+}
+
+void set_motors_direction_reverse() {
+	set_motor_right_direction_reverse();
+	set_motor_left_direction_reverse();
+}
+
+int set_motors_pwm(int pwm) {
+	return set_motor_left_pwm(pwm) && set_motor_right_pwm(pwm);
+}
+
+void disable_motors() {
+	disable_motor_right();
+	disable_motor_left();
+}

21c57ad8e353148d7f9ff6567f830a27c8940872
Ronny Majani <ronnymajani@gmail.com>
2017-09-16 15:47:25 +0300

    Created Motor Driver Files; Started Writing the header

---
+++ main/drivers/motor_driver.c
@@ -0,0 +1,24 @@
+/*
+ * motor_driver.c
+ *
+ *  Created on: Sep 16, 2017
+ *      Author: ronnymajani
+ */
+
+/**
+ * Right Front Motor
+ * Right Back Motor
+ * Left Front Motor
+ * Left Rear Motor
+ *
+ * Right Side (RF + RB)
+ * Left Side (LF + LB)
+ *
+ * Forward(orientation)
+ * Reverse(orientation)
+ * Rotate in place
+ * Break/Stop
+ */
+
+
+

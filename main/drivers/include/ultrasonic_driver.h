4b02f45e48c8c590929301b22f8df9610c5faa31
Ronny Majani <ronnymajani@gmail.com>
2017-09-20 18:42:36 +0300

    Created the Driver's source files.

---
+++ main/drivers/include/ultrasonic_driver.h
@@ -0,0 +1,18 @@
+/*
+ * ultrasonic_driver.h
+ *
+ *  Created on: Sep 20, 2017
+ *      Author: ronnymajani
+ */
+
+#ifndef MAIN_DRIVERS_INCLUDE_ULTRASONIC_DRIVER_H_
+#define MAIN_DRIVERS_INCLUDE_ULTRASONIC_DRIVER_H_
+
+#define ULTRASONIC_DRIVER_GPIO_TRIG  //todo set the correct pin number
+#define ULTRASONIC_DRIVER_GPIO_ECHO  //todo set the correct pin number
+
+/* Exported Functions */
+void init_ultrasonic_driver();
+
+
+#endif /* MAIN_DRIVERS_INCLUDE_ULTRASONIC_DRIVER_H_ */
---
+++ main/drivers/ultrasonic_driver.c
@@ -0,0 +1,8 @@
+/*
+ * ultrasonic_driver.c
+ *
+ *  Created on: Sep 20, 2017
+ *      Author: ronnymajani
+ */
+
+
